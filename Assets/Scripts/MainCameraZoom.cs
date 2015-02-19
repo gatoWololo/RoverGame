bbd775d618335fa0077c7d801617d1feeaf6786c
Scott Hale <scottrhale@gmail.com>
2015-02-19 00:35:49 -0800

    added simple integer zoom to main camera

---
+++ Assets/Scripts/MainCameraZoom.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCameraZoom : MonoBehaviour {
+
+	public float maxZoomOut;
+	public float maxZoomIn;
+	public float currentSize;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentSize = Camera.main.orthographicSize;
+				
+				if (Input.GetAxis ("Mouse ScrollWheel") < 0 && currentSize < maxZoomOut) { // back 
+						Camera.main.orthographicSize++;
+				}
+
+				if (Input.GetAxis ("Mouse ScrollWheel") > 0 && currentSize > maxZoomIn) { // forward
+						Camera.main.orthographicSize--;
+				}
+
+		}
+
+}
