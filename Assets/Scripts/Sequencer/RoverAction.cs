d2b64d774ad24647be78e1503c634fc9dd3fc24e
gatoWololo <gatoWololo@gmail.com>
2015-03-25 06:39:50 -0700

    Added Sequencer functionality for automated moving. Rover now moves based on Rover UI entered commands an pressing play. Check out Sequencer class to see how it works, Mark needs to add the pictures and tiny number as the commnads are added so the UI also reflects the sequence of steps.
    We still need to change it so the rover doesn't stop after every
    tile it touches, this requires some rewriting of RoverMovementScript,
    also major refactoring of RoverMovementScript is needed.

---
+++ Assets/Scripts/Sequencer/RoverAction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Abstract class that all rover actions inheret from.
+/// Rover actions are things like move, where we know the direction
+/// and the number of times to do the action.
+/// </summary>
+abstract public class RoverAction{
+	/// <summary>
+	/// The number of times the action should be performed.
+	/// </summary>
+	protected int actionCounter;
+	//Name of action for the sake of easy comparison.
+	protected string actionName;
+	//Getter for action Counter.
+
+	public int getActionCounter(){
+		return actionCounter;
+	}
+
+	public string getActionName(){
+		return actionName;
+	}
+	//Given an int, will increment the the actionCounter by
+	//that passed integer.
+	public void increaseActionCounter(int amount){
+		actionCounter += amount;
+
+	}
+}
