e00cfa5fc6be0ef86c2debf99e29272535483f76
Scott Hale <scottrhale@gmail.com>
2015-04-01 23:14:21 -0700

    added the UISequencer, modified the backend sequencer just a little bit to allow the UI to query it for status

--- Assets/Scripts/Sequencer/Sequencer.cs
+++ Assets/Scripts/Sequencer/Sequencer.cs
@@ -15,6 +15,12 @@ public class Sequencer : MonoBehaviour {
 	private RoverAction lastAction;
 	// Amount of time to pass before doing next command.
 	float delay = 1.8f;
+
+	// flag for UI to know to dump current Sequence
+	private bool stopFlag;
+	// tracks how many times the stop button has been pushed.
+	private int stopClicks;
+
 	//The Sequencer holds a reference to the rover movement script as it must
 	//talk to it!
 	RoverMovementScript roverMovement;
@@ -24,6 +30,8 @@ public class Sequencer : MonoBehaviour {
 		list = new List<RoverAction> ();
 		lastAction = new EmptyAction();
 		roverMovement = GetComponent<RoverMovementScript> ();
+		stopFlag = false;
+		stopClicks = 0;
 	}
 	//================================================================================
 	// Update is called once per frame
@@ -34,6 +42,7 @@ public class Sequencer : MonoBehaviour {
 	//Called by the ArrowClick and all click functions to add a new command to our
 	//list so later the rover can perfom the list of actions.
 	public void addActionToList(RoverAction action){
+		stopFlag = false;// set the clear flag back to false so the UI knows to display commands again 
 		string lastActionStr = lastAction.getActionName ();
 		string currentActionStr = action.getActionName ();
 
@@ -55,6 +64,7 @@ public class Sequencer : MonoBehaviour {
 	//Wrapper for do actions functions which actually does the work. Needs to be done
 	//this way to appropriately have the delays for the rover.
 	public void doSequence(){
+		stopClicks = 0;
 		StartCoroutine (doActions (list));
 		return;
 	}
@@ -76,6 +86,46 @@ public class Sequencer : MonoBehaviour {
 			//else??? TODO other actions.
 		}
 	}
+
+	public int getLastActionValue(){
+		// This method returns an integer associated with the last action added to the 
+		// list. It is called by the UI in order to populate the sequencer. hale - 03/31/15
+		switch(lastAction.getActionName()){
+			case "": // empty list
+				return 0;
+				break;
+			case "moveUp":
+				return 1;
+				break;
+			case "moveDown":
+				return 2;
+				break;
+			case "moveLeft":
+				return 3;
+				break;
+			case "moveRight":
+				return 4;
+				break;
+			// Add actions here as necessary
+			// Ie Scan, Drill, etc...
+
+			default: //Error
+			return -1;
+		}
+	}
+
+	public int getLastActionQty(){	// Lets the UI know how to set the command subscript
+		return lastAction.getActionCounter ();
+	}
+
+	public int getLengthOfSequence(){ // Lets the UI know how many commands we have in our game list
+		return list.Count;
+	}
+
+	public bool getPlayStatus (){ // Lets the UI know to clear the sequencer
+		return stopFlag;
+	}
+
 	//================================================================================          
 	//These actions are called when the user clicks on the UI for the rover game.
 	public void upArrowClick(){
@@ -103,8 +153,16 @@ public class Sequencer : MonoBehaviour {
 	}
 	//================================================================================
 	//Clears the list field of this class ;)
-	public void clearList(){
-		list.Clear ();
+	public void clearList(){ 
+		// TODO chand the name of this method to reset sequence
+		// This method should stop the rover on one click and clear the list on second click
+		stopClicks++; 
+		if (stopClicks == 2) { // if stop button has been pressed twice we want to clear our list.
+			stopClicks = 0;
+			stopFlag = true;
+			list.Clear ();
+			lastAction = new EmptyAction ();// added so that the UI will know if the list has been cleared. hale - 03/31/15
+		}
 		return;
 	}
 	//================================================================================
---
+++ Assets/Scripts/UI/UISequencer.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class UISequencer : MonoBehaviour {
+
+	private int MAXSEQUENCE = 16;
+
+	private List<GameObject> currentRoutine;
+
+	private int nextAction;
+
+	private int currentLength;
+
+	public Transform FirstSequencerPosition; // a blank GameObject used for parenting the sequencer objects within the sequence grid panel
+
+	GameObject currentCommand;
+
+	private Vector3 vector;
+
+	private Sprite sequenceSprite;
+
+	Sequencer sequencer;
+
+	Object prefab;
+
+
+	void Start () {
+		currentRoutine = new List<GameObject> ();
+		currentLength = 0;
+		nextAction = 0;
+		GameObject roverObject = GameObject.Find("Rover");
+		sequencer = roverObject.GetComponent<Sequencer> ();
+		prefab = Resources.Load("Prefabs/sequencerStepImage", typeof(GameObject));
+		vector = new Vector3 (0f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (sequencer.getPlayStatus ()) {
+			currentCommand = null;
+			resetSequencer();
+		}
+		if (currentLength < MAXSEQUENCE) { // check to see if there is room in the sequencer
+			if (currentLength < sequencer.getLengthOfSequence ()) { // check to see if there has been a command added to the sequence.
+				currentLength++;
+				nextAction = sequencer.getLastActionValue ();
+
+				addActionToUISequence(nextAction);
+			}
+		}
+		if(sequencer.getLastActionQty()>1){
+			setSubscript(sequencer.getLastActionQty(),sequencer.getLastActionValue());
+		}
+
+	}
+
+	private void addActionToUISequence(int nextAction){ 
+	// This method instanciates a new GameObject from prefab, sets the images position within the sequencer grid and then sets 
+	// the image to match the last command entered using the nextAction parameter.
+
+		Debug.Log ("next action is: " + nextAction);
+
+		currentCommand = new GameObject ();
+		currentCommand = Instantiate(prefab, FirstSequencerPosition.position, FirstSequencerPosition.rotation) as GameObject;
+		currentCommand.transform.SetParent(FirstSequencerPosition);
+		calculateGridPosition ();
+		currentCommand.transform.position = currentCommand.transform.position + vector;
+
+		switch (nextAction) {
+			case 1:
+				currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/UpArrow1", typeof(Sprite)) as Sprite;
+				break;
+			case 2:
+			currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/DownArrow1", typeof(Sprite)) as Sprite;
+				break;
+			case 3:
+			currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/LeftArrow1", typeof(Sprite)) as Sprite;
+				break;
+			case 4:
+			currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/RightArrow1", typeof(Sprite)) as Sprite;
+				break;
+		}
+
+		currentRoutine.Add (currentCommand);
+	}
+
+	private void calculateGridPosition(){
+		switch(currentLength%4){
+			case 0:
+				vector.x = 150f;
+				break;
+			case 1:
+				if(currentLength>1){
+					vector.y = vector.y - 40f;
+				}
+				vector.x = 0f;
+				break;
+			case 2:
+				vector.x = 50f;
+				break;
+			case 3:
+				vector.x = 100f;
+				break;
+		}
+	}
+
+	private void setSubscript(int sub, int currentAction){ 
+		// Im sorry about this. Unity will not let me programmatically layer a UI text element on top of a UI image element, So I must 
+		// flip flop images during runtime :(
+		switch (currentAction) {
+		case 1:
+			switch (sub) {
+				case 2:
+					currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/UpArrow2", typeof(Sprite)) as Sprite;
+					break;
+				case 3:
+					currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/UpArrow3", typeof(Sprite)) as Sprite;
+					break;
+				case 4:
+					currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/UpArrow4", typeof(Sprite)) as Sprite;
+					break;
+			}
+			break;
+		case 2:
+			switch (sub) {
+				case 2:
+					currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/DownArrow2", typeof(Sprite)) as Sprite;
+					break;
+				case 3:
+					currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/DownArrow3", typeof(Sprite)) as Sprite;
+					break;
+				case 4:
+					currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/DownArrow4", typeof(Sprite)) as Sprite;
+					break;
+			}
+			break;
+		case 3:
+			switch (sub) {
+				case 2:
+					currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/LeftArrow2", typeof(Sprite)) as Sprite;
+					break;
+				case 3:
+					currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/LeftArrow3", typeof(Sprite)) as Sprite;
+					break;
+				case 4:
+					currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/LeftArrow4", typeof(Sprite)) as Sprite;
+					break;
+			}
+			break;
+		case 4:
+			switch (sub) {
+			case 2:
+				currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/RightArrow2", typeof(Sprite)) as Sprite;
+				break;
+			case 3:
+				currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/RightArrow3", typeof(Sprite)) as Sprite;
+				break;
+			case 4:
+				currentCommand.GetComponent<Image>().overrideSprite = Resources.Load("Textures/RightArrow4", typeof(Sprite)) as Sprite;
+				break;
+			}
+			break;
+		}
+	}
+
+	private void resetSequencer(){
+		foreach (GameObject command in currentRoutine) {
+			Destroy(command);
+		}
+		currentLength = 0;
+		vector.x = 0f;
+		vector.y = 0f;
+	}
+
+	
+}
