c46be1caf0315bb12eedf2b652580767b1729c7a
Scott Hale <scottrhale@gmail.com>
2015-05-05 23:11:35 -0700

    launch menu added, about button and invasion forever button dont work yet.

---
+++ Assets/Scripts/ButtonLogic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonLogic : MonoBehaviour {
+
+	
+
+
+	public void LaunchChosenGame(int gameId){
+		if(gameId == 1){
+			Application.LoadLevel("roverGame");
+		}else{
+			;//LAUNCH INVASION FOREVER
+		}
+	}
+
+	public void killGame(){
+		Application.Quit();
+	}	
+
+}
